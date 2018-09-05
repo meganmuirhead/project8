2ef8d988f1fbc3926720ce1bf97597d20cce7a1f
meganmuirhead <megan@s56.net>
2018-09-04 18:22:50 -0600

    working on adding object to array and printing to /savedUsers:

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 let app = express();
+const fs = require('fs');
+
 const session = require('express-session');
 const uuid = require('uuid/v4')
 const userFile = __dirname + '/user.json';
@@ -14,15 +16,12 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy; // a 'strategy' to use for passport
 
 // const path = require('path');
-// const fs = require('fs');
-const port = process.env.PORT || 3000;
-// // const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-// // const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
-const users = [
-    {id: 0, username: 'jason', password: 'abc', email: 'jason@example.com'},
-    {id: 1, username: 'kate', password: '123', email: 'kate@example.com'}
-];
+const port = process.env.PORT || 3000;
+
+let myJsonArray = {
+    users: []
+};
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -34,15 +33,15 @@ app.get('/', (req, res) => {
     }
     res.render('index', {number,
         title: 'Welcome!',
-        date: new Date(),
-        list: ['apple', 'orange', 'peach']
+        // date: new Date(),
+        // list: ['apple', 'orange', 'peach']
     })
 });
 app.use(cookieParser());
 app.use('/', express.static(__dirname + '/public'));
 
 // app.use('/', express.static('public'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); // use for JSON
 app.use(session({
     store: new FileStore(),
@@ -51,29 +50,87 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.use(bodyParser.urlencoded({extended:false})); //handle body requests
-app.use(bodyParser.json()); // let's make JSON work too!
-
 // app.use(bodyParser.urlencoded({extended: true}));
 app.post('/savedUsers', (req, res) => {
-        console.log('name', req.body.username, 'password', req.body.password, 'email', req.body.email, 'age', req.body.age);
-        res.send('the name you typed is ' + req.body.username + '. You typed ' + req.body.password);
-        // res.sendFile(userFile);
-        // res.sendfile(session({}), req.body.username, (req, res) => {
-            console.log(req.body.username)
-        // res.redirect('/userlistingview');
+        // console.log('name', req.body.username, 'password', req.body.password, 'email', req.body.email, 'age', req.body.age);
+        // res.send('the name you typed is ' + req.body.username + '. You typed ' + req.body.password);
+        // // res.sendFile(userFile);
+        // // res.sendfile(session({}), req.body.username, (req, res) => {
+        //     console.log(req.body.username)
+    let cName = req.body.username;
+    let cPassword = req.body.password;
+    let cEmail = req.body.email;
+    let cAge = req.body.age;
 
-    }
+    fs.readFile('user.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        let index = 0;
+        let pData = JSON.parse(data);
+        console.log(pData);
+        let myDataObject = {
+            id: 0,
+            name: cName,
+            password: cPassword,
+            email: cEmail,
+            age: cAge
+        };
+        pData.users.forEach(client => {
+            if (client.id === index) index++;
+            myJsonArray.users.push(client)
+        });
+        myDataObject.id = index;
+        myJsonArray.users.push(myDataObject);
+        console.log(myDataObject);
+        fs.writeFile(userFile, JSON.stringify(myJsonArray),'utf8', (err) => {
+            if (err) console.log(err);
+        });
+    });
+    res.sendFile(userFile);
+    // res.redirect('/userlistingview');
 
-);
-app.post('/userlistingview', (req, res) => {
-    // res.send(userFile)
-    console.log('name', req.body.username, 'password', req.body.password, 'email', req.body.email, 'age', req.body.age);
 
-    res.send(req.body.username)
 });
-app.use(passport.initialize());
-app.use(passport.session());
+// app.post('/userlistingview', (req, res) => {
+//     // res.send(userFile)
+//     // console.log('name', req.body.username, 'password', req.body.password, 'email', req.body.email, 'age', req.body.age);
+//     //
+//     // res.send(req.body.username)
+//     let cName = req.body.username;
+//     let cPassword = req.body.password;
+//     let cEmail = req.body.email;
+//     let cAge = req.body.age;
+//
+//     fs.readFile('user.json', 'utf8', (err, data) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         let index = 0;
+//         let pData = JSON.parse(data);
+//         console.log(pData);
+//         let myDataObject = {
+//             id: 0,
+//             name: cName,
+//             password: cPassword,
+//             email: cEmail,
+//             age: cAge
+//         };
+//         pData.users.forEach(client => {
+//             if (client.id === index) index++;
+//             myJsonArray.users.push(client)
+//         });
+//         myDataObject.id = index;
+//         myJsonArray.users.push(myDataObject);
+//         console.log(myDataObject);
+//         fs.writeFile(userFile, JSON.stringify(myJsonArray),'utf8', (err) => {
+//             if (err) console.log(err);
+//         });
+//     });
+//     res.sendFile(userFile);
+// });
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 
 app.listen(3000, () => {
