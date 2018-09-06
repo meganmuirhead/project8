85e6bb726c6ec0d2a70cb0ca7072dce6ce6135aa
meganmuirhead <megan@s56.net>
2018-09-06 17:45:41 -0600

    saving with id

--- app.js
+++ app.js
@@ -58,12 +58,20 @@ app.post('/savedUsers', (req, res) => {
         let objectEmail = req.body.email;
         let objectAge = req.body.age;
 
+    function uuidv4() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+        let objectId = uuidv4();
 
         let myReqToObject = {
             name: objectName,
             password: objectPassword,
             email: objectEmail,
-            age: objectAge
+            age: objectAge,
+            id: objectId
         };
         users.push(myReqToObject);
         console.log(users);
