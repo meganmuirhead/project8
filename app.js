2bcd8f0e82334155a7fa1edea1e63156892153b7
meganmuirhead <megan@s56.net>
2018-09-23 19:54:53 -0600

    got it all working

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ const path = require('path');
 let app = express();
 const fs = require('fs');
 
-const session = require('express-session');
+
 const userFile = __dirname + '/user.json';
 
-const FileStore = require('session-file-store')(session);
 const bodyParser = require('body-parser'); // be able to parse form elements
 
 const cookieParser = require('cookie-parser');
 
 
-const port = process.env.PORT || 3000;
 
 let users = [];
 
@@ -34,18 +32,10 @@ app.use('/', express.static(__dirname + '/public'));
 // app.use('/', express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); // use for JSON
-app.use(session({
-    store: new FileStore(),
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true
-}));
+
 
 app.post('/savedUsers', (req, res) => {
-        console.log('name', req.body.username, 'password', req.body.password, 'email', req.body.email, 'age', req.body.age);
-        // res.send('the name you typed is ' + req.body.username + '. You typed ' + req.body.password);
 
-            console.log(req.body.username)
         let objectName = req.body.username;
         let objectPassword = req.body.password;
         let objectEmail = req.body.email;
@@ -66,13 +56,10 @@ app.post('/savedUsers', (req, res) => {
             id: objectId
         };
         users.push(myReqToObject);
-        console.log(users);
         //read and parse json file
         fs.readFile('user.json', 'utf8', (err, data) => {
             if (err) throw err;
-            console.log('data:', data)
             let parsedDataIntoJson = JSON.parse(data);
-            console.log('parsed', parsedDataIntoJson);
             //new object with users added to my data
             parsedDataIntoJson.users.push(myReqToObject);
 
@@ -82,63 +69,89 @@ app.post('/savedUsers', (req, res) => {
     });
     res.redirect('/userlistingview');
 });
-app.get('/userlistingview', (req, res) => {
+
+
+app.post('/editUsersForm', (req, res) => {
+    //open file
     fs.readFile(userFile, 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log('data', data);
-    let parsedData = JSON.parse(data);
-    console.log('parsed', parsedData);
-        // res.send(data);
-        //parse json file into object
-        //take the users property on the object and iterate over my users property
-        // send that to res.render
-        console.log('trying', parsedData.users);
-        res.render('table', {users: parsedData.users})
+        if (err) throw err;
+        let parsedData = JSON.parse(data);
+        let user;
+        for (let i = 0; i < parsedData.users.length; i++) {
+            let id = req.body.id;
+            if( parsedData.users[i].id === id) {
+                user = parsedData.users.splice(i, 1)[0];
+                user.name = req.body.username;
+                user.email = req.body.email;
+                user.password = req.body.password;
+                user.age = req.body.age;
+                break;
+            }
+        }
+        console.log('second try with user', user);
+        if (!user) {
+            res.send('user is not defined');
+            console.log('user in if statmemt', user)
+        }
+        else {
+            console.log('user in else statmemt:', user);
+
+            parsedData.users.push(user);
+            fs.writeFile(userFile, JSON.stringify(parsedData), 'utf8', (err) => {
+                if (err) console.log(err)
+            })
+        }
+
     });
+    res.redirect('/userlistingview');
 });
+
+
 app.get('/editUser', (req, res) => {
     fs.readFile(userFile, 'utf8', (err, data) => {
         if (err) throw err;
-        console.log('data', data);
         let parsedData = JSON.parse(data);
-        console.log('parsed', parsedData);
-        // res.send(data);
-        //parse json file into object
-        //take the users property on the object and iterate over my users property
-        // send that to res.render
-        console.log('trying', parsedData.users);
-        res.render('editUser', {users: parsedData.users})
-    });
-});
-app.get('/delete', (req, res) => {
-    let id = req.query.id;
 
+        let id = req.query.id;
+        let user;
+        for(let i = 0; i < parsedData.users.length; i++){
+            if (parsedData.users[i].id === id){
+               user = parsedData.users[i];
+            }
+        }
+        if(user === null){
+            res.send('Must pass a user!');
+        }
+        res.render('editUser', {user: user});
+
+
+    });
+
+});
+
+app.get('/userlistingview', (req, res) => {
     fs.readFile(userFile, 'utf8', (err, data) => {
         if (err) throw err;
-        console.log('data', data);
         let parsedData = JSON.parse(data);
-        // console.log(parsedData.users.id)
-        // parsedData.users.forEach(e => {
-        //     console.log(e)
-        //     if (id != id) {
-        //         //push to new array if it doesnt match
-        //         //if it does match
-        //         let newArray = parsedData.users.id;
-        //         console.log(newArray)
-        //     }
-        // })
+        res.render('table', {users: parsedData.users})
+    });
+});
+
+app.get('/delete', (req, res) => {
+    fs.readFile(userFile, 'utf8', (err, data) => {
+        let id = req.query.id;
+
+        if (err) throw err;
+        let parsedData = JSON.parse(data);
         for (let i = 0; i < parsedData.users.length; i++) {
             if( parsedData.users[i].id === id) {
                 parsedData.users.splice(i, 1);
                 break;
             }
         }
-        console.log('new users array', parsedData.users);
-        //write array to the user.json file
         fs.writeFile(userFile, JSON.stringify(parsedData), 'utf8', (err) => {
             if (err) console.log(err)
         })
-        //after file gets written to, do a redirect back to the users list
     });
 
     //http://localhost:3000/delete?id=something
